dba8ae1e9ebcfe6f455f904f79072594238b19b8
rm94362@estudante.fieb.edu.br git config --global user.name <programadormovel@gmail.com>
2025-03-19 08:27:55 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,33 @@ function App () {
  
   const [nome, setNome] = useState('Caroline')
  
+  const Formulario = () => {
+  return(
+
+<div>
+
+    <input
+    className= "nome"
+    onChange= {(e)=>{setNome(e.target.value)}}
+    name="nome"
+    type= "text" />
+   <button
+    onClick= {()=>{alert(nome)}}
+    className= "botao">
+    CLIQUE AQUI
+   
+   </button>
+
+</div>
+  );
+  };
   return (
  
 <div>
   <h3>Pizzaria 2c</h3>
-  <input
-  className= "nome"
-  onChange= {(e)=>{setNome(e.target.value)}}
-  name="nome"
-  type= "text" />
- <button
-  onClick= {()=>{alert(nome)}}
-  className= "botao">
-  CLIQUE AQUI
- 
- </button>
+  <Formulario>
+
+  </Formulario>
 </div>
   )
 }
